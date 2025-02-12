7ea9416bd39e68966c18bf6b6d6adbb517e2f6cd
Petrik <112963523+petrikpopov@users.noreply.github.com>
2025-02-13 00:47:24 +0200

    Animation of the whole character

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -1,10 +1,13 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 public class CharacterScript : MonoBehaviour
 {
+    private Animator animator;
      private InputAction moveAction;
     private InputAction jumpAction;
+     private InputAction sprintAction;
     private CharacterController characterController;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
@@ -16,27 +19,35 @@ public class CharacterScript : MonoBehaviour
     {
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        sprintAction = InputSystem.actions.FindAction("Sprint");
+        animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
+        MoveState moveState = MoveState.Idle;
         groundedPlayer = characterController.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        float sprintValue = sprintAction.ReadValue<float>();
         Vector3 cameraForward = Camera.main.transform.forward;
         cameraForward.y = 0;
         if(cameraForward != Vector3.zero)
         {
             cameraForward.Normalize();
         }
-        Vector3 moveStep = playerSpeed * Time.deltaTime * (
+        Vector3 moveStep = playerSpeed * Time.deltaTime * (1+2+sprintValue) * (
             moveValue.x * Camera.main.transform.right +
             moveValue.y * cameraForward
         );
+        if(moveStep.magnitude > 0f) {
+            moveState = sprintValue != 0 ? MoveState.Run : Mathf.Abs(moveValue.x ) >  Mathf.Abs(moveValue.y) ? MoveState.SideWalk : MoveState.Walk; 
+        }
+        this.transform.forward = cameraForward;
         characterController.Move(moveStep);
         // Makes the player jump
         if (jumpAction.ReadValue<float>() > 0 && groundedPlayer)
@@ -46,5 +57,14 @@ public class CharacterScript : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         characterController.Move(playerVelocity * Time.deltaTime);
+
+        animator.SetInteger("MoveState", (int)moveState); 
     }
+}
+
+enum MoveState {
+    Idle = 1,
+    Walk = 2,
+    SideWalk = 3,
+    Run = 4
 }
